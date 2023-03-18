00c97ebdfd35e86d78fd83db2f47e47ce26a7b23
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2023-03-18 17:08:16 -0600

    Tests del componente HomePage añadidos

--- src/09-useContext/HomePage.jsx
+++ src/09-useContext/HomePage.jsx
@@ -9,7 +9,7 @@ export const HomePage = () => {
     return (
         <div>
             <h1>HomePage - <small>{ user?.name }</small></h1>
-            <pre>
+            <pre aria-label="pre">
                 { JSON.stringify( user, null, 3 ) }
             </pre>
         </div>
